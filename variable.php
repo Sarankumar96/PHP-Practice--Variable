ce601834bfc26c3393846347b6aa442b56320054
Saran Kumar <145222035+Sarankumar96@users.noreply.github.com>
2024-04-09 15:10:53 +0530

    added one php file

---
+++ variable.php
@@ -0,0 +1,86 @@
+<?php
+/*
+$num = 5;  
+$num1 = 6;
+echo $num."<br>";  
+$str = "Hello World"; 
+echo $str."<br>"; 
+echo $num + $num1."<br>" ; 
+$float = 1.1;  
+echo $float."<br>" ; 
+$str = "Saran Kumar";  
+echo $str; 
+*/
+?>   
+
+<?php
+/*
+
+function myFn(){
+    $var = 5; 
+    echo "My number is ".$var; 
+} 
+myFn();     
+*/
+?> 
+
+<?php
+
+// $var = "Hello World"; 
+
+// function myFn(){
+//     global $var; 
+//     echo "Variable inside the function ".$var; 
+// } 
+// myfn(); 
+// echo "Variable inside"
+
+?>   
+
+<?php
+// $var = "Hello PHP";  
+// function myFn(){ 
+//     global $var; 
+//     echo $var."<br>";
+// } 
+// myFn();  
+// echo $var;
+?>  
+
+<?php
+// $var = "Saran Kumar";   
+// function myFn(){
+//     global $var; 
+//     echo $var; 
+// }  
+// myFn();  
+// echo "<br>";
+// echo $var; 
+?>  
+
+<?php
+// function myFn(){
+//   static $var = 1; 
+//     echo $var."<br>";
+//     $var++;
+// } 
+// myFn(); 
+// myFn(); 
+// myFn(); 
+?> 
+
+<?php
+// function myFn()
+// {
+//     static $var = 1;
+//     echo $var . "<br>";
+//     $var++;
+// }
+// myFn();
+// myFn();
+// myFn();
+?>
+
+
+
+
